f259481302b09f485e5a3f08b72f12f4421e3459
k5275645 <k5275645@naver.com>
2021-04-21 17:01:19 +0900

    modify....

--- index.js
+++ index.js
@@ -11,6 +11,72 @@ const options = { method: "GET" };
 let KRW = [];
 let BTC = [];
 let USDT = [];
+let markets = [];
+let tempPriceInfo = [];
+
+//clearInterval(timer);
+
+async function getItems(mf) {
+  const getResponse = await fetch(
+    `https://api.upbit.com/v1/ticker?markets=${mf}`,
+    options
+  );
+  const get = await getResponse.json();
+  const tikerInfo = await get[0];
+  return tikerInfo;
+}
+
+function getPrice() {
+  let flagNum = -1;
+  let timer = setInterval(() => {
+    for (i = 0; i < 10; i++) {
+      flagNum > markets.length - 2 ? (flagNum = 0) : flagNum++;
+      const td4 = document.createElement("td");
+      const td5 = document.createElement("td");
+      const td6 = document.createElement("td");
+      const td7 = document.createElement("td");
+      tempPriceInfo.length = 0;
+      getItems(markets[flagNum]).then((res) => {
+        tempPriceInfo.push(res);
+        td4.innerHTML = res.opening_price;
+        td5.innerHTML = res.high_price;
+        td6.innerHTML = res.low_price;
+        td7.innerHTML = res.trade_price;
+      });
+      //console.log(tempPriceInfo);
+      marketTableBody.childNodes[flagNum].appendChild(td4);
+      marketTableBody.childNodes[flagNum].appendChild(td5);
+      marketTableBody.childNodes[flagNum].appendChild(td6);
+      marketTableBody.childNodes[flagNum].appendChild(td7);
+      // if (marketTableBody.childNodes[flagNum].childNodes[3]) {
+      //   marketTableBody.childNodes[flagNum].removeChild(
+      //     marketTableBody.childNodes[flagNum].childNodes[3]
+      //   );
+      // }
+      // marketTableBody.childNodes[flagNum].appendChild(td4);
+      // if (marketTableBody.childNodes[flagNum].childNodes[4]) {
+      //   marketTableBody.childNodes[flagNum].removeChild(
+      //     marketTableBody.childNodes[flagNum].childNodes[4]
+      //   );
+      // }
+      // marketTableBody.childNodes[flagNum].appendChild(td5);
+      // if (marketTableBody.childNodes[flagNum].childNodes[5]) {
+      //   marketTableBody.childNodes[flagNum].removeChild(
+      //     marketTableBody.childNodes[flagNum].childNodes[5]
+      //   );
+      // }
+      // marketTableBody.childNodes[flagNum].appendChild(td6);
+      // if (marketTableBody.childNodes[flagNum].childNodes[6]) {
+      //   marketTableBody.childNodes[flagNum].removeChild(
+      //     marketTableBody.childNodes[flagNum].childNodes[6]
+      //   );
+      // }
+      // marketTableBody.childNodes[flagNum].appendChild(td7);
+
+      // console.log(marketTableBody.childNodes[flagNum].childNodes);
+    }
+  }, 1000);
+}
 
 function appendItem(e) {
   let temp;
@@ -25,16 +91,17 @@ function appendItem(e) {
     marketTableBody.removeChild(marketTableBody.childNodes[0]);
   }
 
+  markets.length = 0;
   temp.forEach((item) => {
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
     const td2 = document.createElement("td");
     const td3 = document.createElement("td");
 
+    markets.push(item.market);
     td1.innerHTML = item.market;
     td2.innerHTML = item.korean_name;
     td3.innerHTML = item.english_name;
-    getPrice(item.market);
 
     tr.appendChild(td1);
     tr.appendChild(td2);
@@ -42,40 +109,12 @@ function appendItem(e) {
 
     marketTableBody.appendChild(tr);
   });
+  getPrice();
 }
 
-// let timer = setInterval(() => {
-//   //clearInterval(timer);
-//   console.log("aa");
-// }, 1000);
-
-async function getPrice(market) {
-  // temp.forEach((item) => {
-  //   console.log(item.market);
-  // });
-  let aa = await fetch(
-    `https://api.upbit.com/v1/ticker?markets=${market}`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((i) => {
-        const td4 = document.createElement("tr");
-        const td5 = document.createElement("tr");
-        const td6 = document.createElement("tr");
-        const td7 = document.createElement("tr");
-        td4.innerHTML = i.opening_price;
-        td5.innerHTML = i.high_price;
-        td6.innerHTML = i.low_price;
-        td7.innerHTML = i.trade_price;
-        tr.appendChild(td4);
-        tr.appendChild(td5);
-        tr.appendChild(td6);
-        tr.appendChild(td7);
-      });
-    })
-    .catch((err) => console.error(err));
-}
+krwBtn.addEventListener("click", appendItem);
+btcBtn.addEventListener("click", appendItem);
+usdtBtn.addEventListener("click", appendItem);
 
 function marketInfoLoad() {
   fetch("https://api.upbit.com/v1/market/all?isDetails=false", options)
@@ -84,21 +123,18 @@ function marketInfoLoad() {
     })
     .then((data) => {
       data.forEach((item) => {
-        if (item.market.split("-")[0] === "KRW") {
+        let splitMarket = item.market.split("-");
+        if (splitMarket[0] === "KRW") {
           KRW.push(item);
-        } else if (item.market.split("-")[0] === "BTC") {
+        } else if (splitMarket[0] === "BTC") {
           BTC.push(item);
-        } else if (item.market.split("-")[0] === "USDT") {
+        } else if (splitMarket[0] === "USDT") {
           USDT.push(item);
         }
       });
     })
     .catch((err) => console.error(err));
 }
-
-krwBtn.addEventListener("click", appendItem);
-btcBtn.addEventListener("click", appendItem);
-usdtBtn.addEventListener("click", appendItem);
 
 function init() {
   marketInfoLoad();

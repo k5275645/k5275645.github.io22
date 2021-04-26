617bcf2c6fec1350139c9a1f0ea2a9c5757a7d66
k5275645 <k5275645@naver.com>
2021-04-26 15:22:35 +0900

    semi final

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 변수 세팅
 const container = document.querySelector(".container");
 const krwBtn = document.querySelector(".KRW");
 const btcBtn = document.querySelector(".BTC");
@@ -19,10 +20,32 @@ interestTable.style.display = "none";
 
 //clearInterval(timer);
 
+// 회계식 표현 정규식
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+// 관심목록 추가/제거
+function addInterestList(e) {
+  let cloneItem = e.path[1].cloneNode(true);
+  cloneItem.firstChild.disabled = true;
+
+  if (e.path[0].checked) {
+    interestTableBody.appendChild(cloneItem);
+  } else {
+    interestTableBody.childNodes.forEach((item) => {
+      if (e.path[1].id === item.id) {
+        interestTableBody.removeChild(item);
+      }
+    });
+  }
+
+  interestTableBody.childNodes.length === 0
+    ? (interestTable.style.display = "none")
+    : (interestTable.style.display = "block");
+}
+
+// 가격정보를 가져와서 표에 뿌리기
 async function getItems(mf, flagNum) {
   const getResponse = await fetch(
     `https://api.upbit.com/v1/ticker?markets=${mf}`,
@@ -30,36 +53,40 @@ async function getItems(mf, flagNum) {
   );
   const get = await getResponse.json();
   const tikerInfo = await get[0];
+  //console.log(tikerInfo.acc_trade_price);
+  //console.log(tikerInfo.acc_trade_volume_24h);
   marketTableBody.childNodes[
     flagNum
-  ].childNodes[4].innerText = numberWithCommas(tikerInfo.opening_price);
+  ].childNodes[2].innerText = numberWithCommas(tikerInfo.opening_price);
   marketTableBody.childNodes[
     flagNum
-  ].childNodes[5].innerText = numberWithCommas(tikerInfo.high_price);
+  ].childNodes[3].innerText = numberWithCommas(tikerInfo.high_price);
   marketTableBody.childNodes[
     flagNum
-  ].childNodes[6].innerText = numberWithCommas(tikerInfo.low_price);
+  ].childNodes[4].innerText = numberWithCommas(tikerInfo.low_price);
   marketTableBody.childNodes[
     flagNum
-  ].childNodes[7].innerText = numberWithCommas(tikerInfo.trade_price);
-  marketTableBody.childNodes[flagNum].childNodes[8].innerText =
+  ].childNodes[5].innerText = numberWithCommas(tikerInfo.trade_price);
+  marketTableBody.childNodes[flagNum].childNodes[6].innerText =
     ((tikerInfo.trade_price / tikerInfo.opening_price - 1) * 100).toFixed(2) +
     "%";
 
-  marketTableBody.childNodes[flagNum].childNodes[4].style.textAlign = "right";
-  marketTableBody.childNodes[flagNum].childNodes[5].style.textAlign = "right";
-  marketTableBody.childNodes[flagNum].childNodes[6].style.textAlign = "right";
-  marketTableBody.childNodes[flagNum].childNodes[7].style.textAlign = "right";
+  marketTableBody.childNodes[
+    flagNum
+  ].childNodes[7].innerText = numberWithCommas(
+    tikerInfo.acc_trade_price_24h.toFixed()
+  );
 
   if (tikerInfo.opening_price < tikerInfo.trade_price) {
-    marketTableBody.childNodes[flagNum].childNodes[7].style.color = "red";
-    marketTableBody.childNodes[flagNum].childNodes[8].style.color = "red";
+    marketTableBody.childNodes[flagNum].childNodes[5].style.color = "red";
+    marketTableBody.childNodes[flagNum].childNodes[6].style.color = "red";
   } else if (tikerInfo.opening_price > tikerInfo.trade_price) {
-    marketTableBody.childNodes[flagNum].childNodes[7].style.color = "blue";
-    marketTableBody.childNodes[flagNum].childNodes[8].style.color = "blue";
+    marketTableBody.childNodes[flagNum].childNodes[5].style.color = "blue";
+    marketTableBody.childNodes[flagNum].childNodes[6].style.color = "blue";
   }
 }
 
+// 실시간으로 가격정보 가져오기(초당 10개 limit)
 function getPrice() {
   let flagNum = -1;
   let timer = setInterval(() => {
@@ -70,25 +97,7 @@ function getPrice() {
   }, 1000);
 }
 
-function addInterestList(e) {
-  let cloneItem = e.path[1].cloneNode(true);
-  cloneItem.firstChild.disabled = true;
-  // console.log(cloneItem);
-  //console.log(interestTableBody.childNodes);
-  if (e.path[0].checked) {
-    interestTableBody.appendChild(cloneItem);
-  } else {
-    interestTableBody.childNodes.forEach((item) => {
-      if ((e.path[1] = item)) {
-        interestTableBody.removeChild(item);
-      }
-    });
-  }
-  interestTableBody.childNodes.length === 0
-    ? (interestTable.style.display = "none")
-    : (interestTable.style.display = "block");
-}
-
+// 마켓별 정보를 출력할 표 세팅
 function appendItem(e) {
   marketTable.style.display = "block";
   let temp;
@@ -115,14 +124,12 @@ function appendItem(e) {
     const td5 = document.createElement("td");
     const td6 = document.createElement("td");
     const td7 = document.createElement("td");
-    const td8 = document.createElement("td");
+
     checkBox.type = "checkBox";
     checkBox.addEventListener("change", addInterestList);
 
     markets.push(item.market);
-    td1.innerHTML = item.market;
-    td2.innerHTML = item.korean_name;
-    td3.innerHTML = item.english_name;
+    td1.innerHTML = item.korean_name;
     tr.id = trId;
     tr.appendChild(checkBox);
     tr.appendChild(td1);
@@ -132,7 +139,6 @@ function appendItem(e) {
     tr.appendChild(td5);
     tr.appendChild(td6);
     tr.appendChild(td7);
-    tr.appendChild(td8);
 
     marketTableBody.appendChild(tr);
   });
@@ -143,6 +149,7 @@ krwBtn.addEventListener("click", appendItem);
 btcBtn.addEventListener("click", appendItem);
 usdtBtn.addEventListener("click", appendItem);
 
+// 모든 코인 이름 가져오기
 function marketInfoLoad() {
   fetch("https://api.upbit.com/v1/market/all?isDetails=false", options)
     .then((response) => {
@@ -163,6 +170,7 @@ function marketInfoLoad() {
     .catch((err) => console.error(err));
 }
 
+// 시작
 function init() {
   marketInfoLoad();
 }
